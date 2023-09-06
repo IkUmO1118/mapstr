34eae1130edfbeaf533fd6e776094b6ed5ad23b9
IkUmO1118 <ikumo1118free@gmail.com>
2023-09-06 22:37:33 +0900

    Fixed layer

--- src/js/script.js
+++ src/js/script.js
@@ -259,6 +259,9 @@ class App {
 
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
+    // L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    //   maxZoom: 18,
+    //   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
